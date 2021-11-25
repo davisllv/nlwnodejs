6678f81c5bb54cda5ee9d68403549d46667b4b11
davisllv <davisllvsnt57@gmail.com>
2021-11-25 20:31:11 -0300

    fiveth day

---
+++ src/controllers/ListUserSendComplimentsController.ts
@@ -0,0 +1,11 @@
+import { Request,Response } from "express";
+
+
+class ListUserSendComplimentsController {
+async handle(request: Request, response: Response){
+  const listUserSendComp
+}
+
+}
+
+export { ListUserSendComplimentsController }
